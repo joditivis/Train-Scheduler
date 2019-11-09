2cacb0ff99e6cf8ee0ecfe2a978b38573200a52e
Jodi Tivis <joditivis@gmail.com>
2019-11-08 19:23:08 -0700

    added firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// my web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDzJk-N1KIMrnYZoTe9e_nK6lN45skKrWY",
+    authDomain: "train-scheduler-4e898.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4e898.firebaseio.com",
+    projectId: "train-scheduler-4e898",
+    storageBucket: "train-scheduler-4e898.appspot.com",
+    messagingSenderId: "255012387202",
+    appId: "1:255012387202:web:a8ac5d41b9fbdb1149379b"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
